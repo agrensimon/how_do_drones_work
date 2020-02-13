f7c5429e31f0114f86dedce380a3a53bb0184564
agrensimon <simon.agren@gmail.com>
2020-02-13 15:36:34 +0100

    aruco pose estimation and control

--- opencv/aruco_pose_estimation.py
+++ opencv/aruco_pose_estimation.py
@@ -38,11 +38,13 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import sys, time, math
+import cflib as cf
+
+print("OpenCV version: ",cv2.__version__)
 
 #--- Define Tag
 id_to_find  = 0
 marker_size  = 13.23 #- [cm]
-
 
 #------------------------------------------------------------------------------
 #------- ROTATIONS https://www.learnopencv.com/rotation-matrix-to-euler-angles/
@@ -77,13 +79,30 @@ def rotationMatrixToEulerAngles(R):
 
     return np.array([x, y, z])
 
-
+def drawCrosshair(frame):
+    cv2.drawMarker(frame, (int(resolution[0]/2),int(resolution[1]/2)),(0,0,0),markerType=cv2.MARKER_CROSS, thickness=2)
 
 
 #--- Get the camera calibration path
-calib_path  = ""
-camera_matrix   = np.loadtxt(calib_path+'cameraMatrix_webcam.txt', delimiter=',')
-camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_webcam.txt', delimiter=',')
+
+def loadCameraParams(cam_name):
+    if cam_name is 'runcam_nano3':
+        camera_matrix = np.array([[269.11459175467655, 0.0, 318.8896785174727], [0.0, 262.62554966204, 248.54894259248005], [0.0, 0.0, 1.0]])
+        camera_dist = np.array([[-0.1913802179616581, 0.04076781232772304, -0.0014647104190866982, 0.00047321030718756505, -0.0043907605166862065]])
+        calibration_error = 0.4467944666063116
+    elif cam_name is 'runcam_nanolth':
+        camera_matrix = np.array([[333.1833852111547, 0.0, 327.7723204462851], [0.0, 337.3376244908818, 229.96013817983925], [0.0, 0.0, 1.0]])
+        camera_dist = np.array([[-0.37915663345130246, 0.18478180306843126, -0.00021990379249122642, -0.0014864903771132248, -0.05061040147030076]])
+        calibration_error = 0.5077483684005625
+    else:
+        print('Camera not found. Returning shit values.')
+        camera_matrix = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+        camera_dist = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]])
+        calibration_error = 100
+
+    return camera_matrix, camera_dist, calibration_error
+
+camera_matrix, camera_distortion, _   = loadCameraParams('runcam_nanolth')
 
 #--- 180 deg rotation matrix around the x axis
 R_flip  = np.zeros((3,3), dtype=np.float32)
@@ -97,10 +116,8 @@ parameters  = aruco.DetectorParameters_create()
 
 
 #--- Capture the videocamera (this may also be a video or a picture)
-cap = cv2.VideoCapture(0)
-#-- Set the camera size as the one it was calibrated with
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap = cv2.VideoCapture(2)
+resolution = (640, 480)
 
 #-- Font for the text in the image
 font = cv2.FONT_HERSHEY_PLAIN
@@ -117,24 +134,30 @@ while True:
     corners, ids, rejected = aruco.detectMarkers(image=gray, dictionary=aruco_dict, parameters=parameters,
                               cameraMatrix=camera_matrix, distCoeff=camera_distortion)
     
-    if ids is not None and ids[0] == id_to_find:
+    if ids is not None: # and ids[0] == id_to_find:
         
         #-- ret = [rvec, tvec, ?]
         #-- array of rotation and position of each marker in camera frame
         #-- rvec = [[rvec_1], [rvec_2], ...]    attitude of the marker respect to camera frame
         #-- tvec = [[tvec_1], [tvec_2], ...]    position of the marker in camera frame
-        ret = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
 
+        rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
+        
         #-- Unpack the output, get only the first
-        rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
+        rvec, tvec = rvecs[0,0,:], tvecs[0,0,:]
 
         #-- Draw the detected marker and put a reference frame over it
         aruco.drawDetectedMarkers(frame, corners)
-        aruco.drawAxis(frame, camera_matrix, camera_distortion, rvec, tvec, 10)
+
+        drawCrosshair(frame)
+        
+        # Draw the detected markers axis
+        for i in range(len(rvecs)):
+            aruco.drawAxis(frame, camera_matrix, camera_distortion, rvecs[i,0,:], tvecs[i,0,:], 6.5)
 
         #-- Print the tag position in camera frame
-        str_position = "MARKER Position x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
-        cv2.putText(frame, str_position, (0, 100), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        #str_position = "MARKER Position x=%4.0f  y=%4.0f  z=%4.0f"%(tvec[0], tvec[1], tvec[2])
+        #cv2.putText(frame, str_position, (0, 100), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         #-- Obtain the rotation matrix tag->camera
         R_ct    = np.matrix(cv2.Rodrigues(rvec)[0])
@@ -144,26 +167,23 @@ while True:
         roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip*R_tc)
 
         #-- Print the marker's attitude respect to camera frame
-        str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),math.degrees(pitch_marker),
-                            math.degrees(yaw_marker))
-        cv2.putText(frame, str_attitude, (0, 150), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        #str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),math.degrees(pitch_marker),
+                            #math.degrees(yaw_marker))
+        #cv2.putText(frame, str_attitude, (0, 150), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
 
-        #-- Now get Position and attitude f the camera respect to the marker
+        #-- Now get Position and attitude of the camera respect to the marker
         pos_camera = -R_tc*np.matrix(tvec).T
-
-        str_position = "CAMERA Position x=%4.0f  y=%4.0f  z=%4.0f"%(pos_camera[0], pos_camera[1], pos_camera[2])
-        cv2.putText(frame, str_position, (0, 200), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        str_position = "Position error: x=%4.4f  y=%4.4f  z=%4.4f"%(pos_camera[0], pos_camera[1], pos_camera[2])
+        cv2.putText(frame, str_position, (0, 20), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
         #-- Get the attitude of the camera respect to the frame
         roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(R_flip*R_tc)
-        str_attitude = "CAMERA Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_camera),math.degrees(pitch_camera),
+        str_attitude = "Anglular error: roll=%4.4f  pitch=%4.4f  yaw (z)=%4.4f"%(math.degrees(roll_camera),math.degrees(pitch_camera),
                             math.degrees(yaw_camera))
-        cv2.putText(frame, str_attitude, (0, 250), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, str_attitude, (0, 40), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
-
-
-    
+        cv2.ellipse(frame, (20,400),(10,10), 0, 0, math.degrees(yaw_camera), (0,0,0), thickness=3)
 
 
     #--- Display the frame
